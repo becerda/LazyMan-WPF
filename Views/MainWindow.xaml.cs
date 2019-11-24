2c2b113dd58b0663662b8f94bdadab59c901a7f6
becerda <benjamin.cerda773@myci.csuci.edu>
2019-11-23 23:28:27 -0800

    Branch MainWindowDesign 	- MainWindow.xaml 		- NHL layout updated 		- MLB layout updated 		- functionality not implemented yet 	- MainWindow.xaml.cs 		- Initialization logic for MainWindow.xaml

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LazyManWPF.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(object dataContext)
+        {
+            InitializeComponent();
+            DataContext = dataContext;
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            /*if(DataContext is IClosable)
+            {
+                (DataContext as IClosable).RequestClose += (_, __,) => this.Close();
+            }*/
+        }
+    }
+}
