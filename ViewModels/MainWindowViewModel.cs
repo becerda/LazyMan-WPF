9599cc35998711a5c9de05d014ee389f0e3ec93e
becerda <benjamin.cerda773@myci.csuci.edu>
2019-11-24 00:23:37 -0800

    Branch MainWindowViewModel 	- MainWindowViewModel.cs 		- Initial file commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyManWPF.ViewModels
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+
+        protected void OnPropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
